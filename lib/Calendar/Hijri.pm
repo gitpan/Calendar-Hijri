1fd7c73bbc77ad4782b43de4f3255b8e253defc9
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2011-04-19 08:29:07 +0000

    Import of MANWAR/Calendar-Hijri-0.04 from CPAN.
    gitpan-cpan-distribution: Calendar-Hijri
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         MANWAR/Calendar-Hijri-0.04.tar.gz
    gitpan-cpan-author:       MANWAR
    gitpan-cpan-maturity:     released

--- lib/Calendar/Hijri.pm
+++ lib/Calendar/Hijri.pm
@@ -5,15 +5,15 @@ use strict;
 
 =head1 NAME
 
-Calendar::Hijri - Interface to Islamic calendar.
+Calendar::Hijri - Interface to Islamic Calendar.
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Carp;
 use Readonly;
@@ -30,7 +30,7 @@ Readonly my $GREGORIAN_EPOCH => 1721425.5;
 Readonly my $MONTHS =>
 [
     undef,
-    q/Muharram/, q/Safar/   , q/Rabi' al-awwal/, q/Rabi' al-thani/, q/Jumada al-awwal/,  q/Jumada al-thani/,  
+    q/Muharram/, q/Safar/   , q/Rabi' al-awwal/, q/Rabi' al-thani/, q/Jumada al-awwal/,  q/Jumada al-thani/,
     q/Rajab/   , q/Sha'aban/, q/Ramadan/       , q/Shawwal/       , q/Dhu al-Qi'dah/   , q/Dhu al-Hijjah/  ,
 ];
 
@@ -44,7 +44,7 @@ sub new
     my $dd    = shift;
     my $self  = {};
     bless $self, $class;
-    
+
     if (defined($yyyy) && defined($mm) && defined($dd))
     {
         _validate_date($yyyy, $mm, $dd)
@@ -57,21 +57,21 @@ sub new
     $self->{yyyy} = $yyyy;
     $self->{mm}   = $mm;
     $self->{dd}   = $dd;
-    
+
     return $self;
 }
 
 =head1 DESCRIPTION
 
-Hijri Calendar begins with the migration from Mecca to Medina of Mohammad (pbuh),  the Prophet 
-of Islam, an event  known  as the Hegira. The initials A.H.  before a date mean "anno Hegirae" 
-or "after Hegira".  The  first  day  of the year is fixed in the Quran as the first day of the 
-month of Muharram. In 17 AH Umar I, the second caliph, established the beginning of the era of 
+Hijri Calendar begins with the migration from Mecca to Medina of Mohammad (pbuh),  the Prophet
+of Islam, an event  known  as the Hegira. The initials A.H.  before a date mean "anno Hegirae"
+or "after Hegira".  The  first  day  of the year is fixed in the Quran as the first day of the
+month of Muharram. In 17 AH Umar I, the second caliph, established the beginning of the era of
 the Hegira (1 Muharram 1 AH) as the date that is 16 July 622 CE in the Julian Calendar.
 
-The  years are lunar and consist of 12 lunar months. There is no intercalary period, since the 
-Quran ( Sura IX, verses 36,37 )  sets  the calendar year at 12 months. Because the year in the 
-Hijri  calendar is shorter than a solar year, the months drift with respect to the seasons, in 
+The  years are lunar and consist of 12 lunar months. There is no intercalary period, since the
+Quran ( Sura IX, verses 36,37 )  sets  the calendar year at 12 months. Because the year in the
+Hijri  calendar is shorter than a solar year, the months drift with respect to the seasons, in
 a cycle 32.50 years.
 
 NOTE: The Hijri date produced by this module can have +1/-1 day error.
@@ -86,7 +86,7 @@ NOTE: The Hijri date produced by this module can have +1/-1 day error.
     |   3    | Rabi' al-awwal  |
     |   4    | Rabi' al-thani  |
     |   5    | Jumada al-awwal |
-    |   6    | Jumada al-thani |  
+    |   6    | Jumada al-thani |
     |   7    | Rajab           |
     |   8    | Sha'aban        |
     |   9    | Ramadan         |
@@ -124,7 +124,7 @@ Return Hijri date in human readable format.
 
     use strict; use warnings;
     use Calendar::Hijri;
-    
+
     my $calendar = Calendar::Hijri->new(1432, 7, 27);
     print "Hijri date is " . $calendar->as_string() . "\n";
 
@@ -142,7 +142,7 @@ Return 1 or 0 depending on whether the given year is a leap year or not in Hijri
 
     use strict; use warnings;
     use Calendar::Hijri;
-    
+
     my $calendar = Calendar::Hijri->new(1432, 7, 27);
     ($calendar->is_leap_year())
     ?
@@ -157,9 +157,9 @@ sub is_leap_year
     my $self = shift;
     my $yyyy = shift;
     $yyyy = $self->{yyyy} unless defined $yyyy;
-    
+
     return unless defined $yyyy;
-    
+
     my $mod = $yyyy%30;
     return 1 if grep/$mod/,@$LEAP_YEAR_MOD;
     return 0;
@@ -171,7 +171,7 @@ Returns the number of days in the given year of Hijri Calendar.
 
     use strict; use warnings;
     use Calendar::Hijri;
-    
+
     my $calendar = Calendar::Hijri->new(1432, 7, 27);
     print "Total number of days in year 1432: " . $calendar->days_in_year() . "\n";
 
@@ -182,9 +182,9 @@ sub days_in_year
     my $self = shift;
     my $yyyy = shift;
     $yyyy = $self->{yyyy} unless defined $yyyy;
-    
+
     return unless defined $yyyy;
-    
+
     ($self->is_leap_year($yyyy))
     ?
     (return 355)
@@ -211,12 +211,12 @@ sub days_in_month
     my $self = shift;
     my $yyyy = shift;
     my $mm   = shift;
-    
+
     $mm = $self->{mm}     unless defined $mm;
     $yyyy = $self->{yyyy} unless defined $yyyy;
-    
+
     return unless (defined($mm) && defined($yyyy));
-    
+
     return 30 if (($mm%2 == 1) || (($mm == 12) && ($self->is_leap_year($yyyy))));
     return 29;
 }
@@ -225,7 +225,7 @@ sub days_in_month
 
     use strict; use warnings;
     use Calendar::Hijri;
-    
+
     my $calendar = Calendar::Hijri->new(1432, 7, 27);
     print "Days before 01 Rajab 1432: " . $calendar->days_so_far() . "\n";
 
@@ -236,11 +236,11 @@ sub days_so_far
     my $self = shift;
     my $yyyy = shift;
     my $mm   = shift;
-    
+
     $mm   = $self->{mm}   unless defined $mm;
     $yyyy = $self->{yyyy} unless defined $yyyy;
     return unless (defined($mm) && defined($yyyy));
-    
+
     my $days = 0;
     foreach (1..$mm) 
     {
@@ -267,13 +267,13 @@ sub add_day
     my $dd    = shift;
     my $mm    = shift;
     my $yyyy  = shift;
-    
+
     $dd   = $self->{dd}   unless defined $dd;
     $mm   = $self->{mm}   unless defined $mm;
     $yyyy = $self->{yyyy} unless defined $yyyy;
 
     return unless (defined($dd) && defined($mm) && defined($yyyy));
-    
+
     foreach (1..$day)
     {
         ($dd, $mm, $yyyy) = _add_day($self->days_in_month($yyyy, $mm), $dd, $mm, $yyyy);
@@ -283,7 +283,7 @@ sub add_day
 
 =head2 get_calendar()
 
-Return  Hijri  Calendar  for the given month and year. In case of missing  month  and year, it 
+Return  Hijri  Calendar  for the given month and year. In case of missing  month  and year, it
 would return current month Hijri Calendar.
 
     use strict; use warnings;
@@ -299,7 +299,7 @@ sub get_calendar
     my $self = shift;
     my $yyyy = shift;    
     my $mm   = shift;
-    
+
     $yyyy = $self->{yyyy} unless defined $yyyy;    
     $mm   = $self->{mm}   unless defined $mm;
 
@@ -335,7 +335,7 @@ sub from_gregorian
     my $yyyy = shift;
     my $mm   = shift;
     my $dd   = shift;
-    
+
     return $self->from_julian(_gregorian_to_julian($yyyy, $mm, $dd));
 }
 
@@ -361,7 +361,7 @@ sub to_gregorian
     $yyyy = $self->{yyyy} unless defined $yyyy;
     $mm   = $self->{mm}   unless defined $mm;
     $dd   = $self->{dd}   unless defined $dd;
-    
+
     return _julian_to_gregorian($self->to_julian($yyyy, $mm, $dd));
 }
 
@@ -382,11 +382,11 @@ sub to_julian
     my $yyyy = shift;
     my $mm   = shift;
     my $dd   = shift;
-    
+
     $yyyy = $self->{yyyy} unless defined $yyyy;
     $mm   = $self->{mm}   unless defined $mm;
     $dd   = $self->{dd}   unless defined $dd;
-    
+
     return ($dd +
             ceil(29.5 * ($mm - 1)) +
             ($yyyy - 1) * 354 +
@@ -403,7 +403,7 @@ sub from_julian
     my $yyyy = floor(((30 * ($julian - $ISLAMIC_EPOCH)) + 10646) / 10631);
     my $mm   = min(12, ceil(($julian - (29 + $self->to_julian($yyyy, 1, 1))) / 29.5) + 1);
     my $dd   = ($julian - $self->to_julian($yyyy, $mm, 1)) + 1;
-    
+
     return ($yyyy, $mm, $dd);
 }
 
@@ -412,16 +412,16 @@ sub start_index
     my $self = shift;
     my $yyyy = shift;
     my $mm   = shift;
-    
+
     $yyyy = $self->{yyyy} unless defined $yyyy;    
     $mm   = $self->{mm}   unless defined $mm;
-    
+
     my ($g_y, $g_m, $g_d) = $self->to_gregorian($yyyy, 1, 1);
     my $dow = Day_of_Week($g_y, $g_m, $g_d);
-    
+
     return $dow if $mm == 1;
     my $days = $self->days_so_far($yyyy, $mm-1);
-    
+
     for (1..$days)
     {
         if ($dow != 6)
@@ -493,7 +493,7 @@ sub _add_day
     my $mm   = shift;
     my $yyyy = shift;
     return unless (defined($dd) && defined($mm) && defined($yyyy));
-    
+
     $dd++;
     if ($dd >= 29)
     {
@@ -532,8 +532,8 @@ Mohammad S Anwar, C<< <mohammad.anwar at yahoo.com> >>
 =head1 BUGS
 
 Please report any bugs or feature requests to C<bug-calendar-hijri at rt.cpan.org>, or through
-the  web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Calendar-Hijri>. I will 
-be  notified ,  and  then  you'll  automatically be notified of progress on your bug as I make 
+the  web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Calendar-Hijri>. I will
+be  notified ,  and  then  you'll  automatically be notified of progress on your bug as I make
 changes.
 
 =head1 SUPPORT
@@ -569,14 +569,14 @@ L<http://search.cpan.org/dist/Calendar-Hijri/>
 Copyright 2011 Mohammad S Anwar.
 
 This  program  is  free  software; you can redistribute it and/or modify it under the terms of
-either :  the  GNU General Public License as published by the Free Software Foundation; or the 
+either :  the  GNU General Public License as published by the Free Software Foundation; or the
 Artistic License.
 
 See http://dev.perl.org/licenses/ for more information.
 
 =head1 DISCLAIMER
 
-This  program  is  distributed  in  the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+This  program  is  distributed  in  the hope that it will be useful, but WITHOUT ANY WARRANTY;
 without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 =cut
